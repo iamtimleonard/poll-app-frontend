0f62c37c32288c3d569f7edcec6bf725678dd863
iamtimleonard <timleonardcello@gmail.com>
2021-02-22 10:19:09 -0500

    optimize for heroku procfile

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PollsList from "./components/PollsList";
 import NewPoll from "./components/NewPoll";
 import axios from "axios";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-let port = process.env.PORT || "http://localhost:5000";
+let API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 const App = () => {
   const [polls, setPolls] = useState([]);
@@ -18,7 +19,7 @@ const App = () => {
   }, []);
 
   const fetchPolls = async () => {
-    const res = await fetch(`${port}/polls`);
+    const res = await fetch(`${API_URL}/polls`);
     const data = await res.json();
     return data;
   };
@@ -32,14 +33,14 @@ const App = () => {
     });
 
     axios
-      .post(`${port}/polls/vote/${pollId}`, pollFromState)
+      .post(`${API_URL}/polls/vote/${pollId}`, pollFromState)
       .then((res) => console.log(res.data));
 
     setPolls([pollFromState, ...remainingPolls]);
   };
 
   const handleCreate = (pollData) => {
-    axios.post(`${port}/polls/add`, pollData).then((res) => {
+    axios.post(`${API_URL}/polls/add`, pollData).then((res) => {
       pollData._id = res.data;
       setPolls([...polls, pollData]);
     });
